3d874c87dd9587495c16f30d21356a9355022b98
arno <1@galliker.info>
2018-04-20 16:03:22 +0200

    add new page "ShoppingList" (as lazy loaded module), set as start page, remove old home page

---
+++ src/pages/shopping-list/shopping-list.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ShoppingListPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage({
+  segment: "list"
+})
+@Component({
+  selector: 'page-shopping-list',
+  templateUrl: 'shopping-list.html',
+})
+export class ShoppingListPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  static ionViewDidLoad() {
+    console.log('ionViewDidLoad ShoppingListPage');
+  }
+
+}
