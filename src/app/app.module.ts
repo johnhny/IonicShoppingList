237dd4b35984049a8b2c8eb2548b900986b91de8
arno <1@galliker.info>
2018-05-04 09:08:19 +0200

    ionic g provider bringmeister

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
+import { BringmeisterProvider } from '../providers/bringmeister/bringmeister';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { MyApp } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    BringmeisterProvider
   ]
 })
 export class AppModule {}
